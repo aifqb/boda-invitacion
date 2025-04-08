90d193b0ee156cc3f3782c46a6ab1b581f7afcb4
alex francesc <76713839+aifqb@users.noreply.github.com>
2025-04-08 16:41:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import BodaInfo from './BodaInfo';
-// Puedes importar el archivo CSS de App.js aquí si lo creas
-// import './App.css';
+// Puedes crear otros componentes para otras rutas si es necesario
 
 function App() {
   return (
-    <div>
-      <BodaInfo />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/invitacion" element={<BodaInfo />} />
+        {/* Redirecciona la ruta raíz a /invitacion */}
+        <Route path="/" element={<Navigate to="/invitacion" replace />} />
+        {/* Puedes añadir más rutas aquí si tu aplicación crece */}
+        {/* <Route path="/otra-pagina" element={<OtroComponente />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
